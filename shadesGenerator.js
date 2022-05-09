8edb9c09229c7132b1a74ccd69b7749a8a4f256e
Sneha Parande <99521196+snehaparande@users.noreply.github.com>
2022-05-09 15:52:02 +0530

    Genrerates an html page having head, title and body. 	extracted functions 'shadesGenerator', 'createPage', 'generateTag' and 'link'.

--- shadesGenerator.js
+++ shadesGenerator.js
@@ -1,4 +1,23 @@
 const { writeFileSync } = require('fs');
 
-const htmlCode = '';
-writeFileSync('index.html', htmlCode, 'utf8');
+const generateTag = function(tag, content){
+  return '<' + tag + '>' + content + '</' + tag + '>';
+};
+
+const link = function(){
+  return '<link rel="stylesheet" href="styles.css"/>';
+};
+
+const createPage = function () {
+  const headContent = generateTag('title', 'Color Shades') + link();
+  const head = generateTag('head', headContent);
+  const body = generateTag('body', '');
+  return generateTag('html', head + body);
+};
+
+const shadesGenerator = function () {
+  const htmlCode = createPage();
+  writeFileSync('index.html', htmlCode, 'utf8');
+};
+
+shadesGenerator();
