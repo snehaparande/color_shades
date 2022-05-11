9a91ffcba42138c2761c883bd02ff4ffc5bd7d85
Sneha Parande <99521196+snehaparande@users.noreply.github.com>
2022-05-11 10:34:49 +0530

    refactored code.

--- shadesGenerator.js
+++ shadesGenerator.js
@@ -10,40 +10,59 @@ const closingTag = tag => '</' + tag + '>';
 
 const classAttribute = (value) => 'class="' + value + '"';
 
-const style = function (rgb, alpha) {
+const bg = ({ rgb, alpha }) => {
   const color = rgb.join(',');
-  return 'style= "background-color: rgba(' + color + ',' + alpha + ')"';
+  return 'background-color: rgba(' + color + ',' + alpha + ')';
 };
 
-const link = () => '<link rel="stylesheet" href="styles.css"/>';
-
-const createTag = function(tag, attribute, content){
-  return openingTag(tag, classAttribute(attribute)) + content + closingTag(tag);
+const attribute = ({ attr, value }) => {
+  return attr + '="' + value + '"';
 };
 
-const shade = (rgb, alpha) => {
-  const allAttributes = classAttribute('shade') + style(rgb, alpha);
-  return openingTag('div', allAttributes) + closingTag('div');
+const attributes = (attrs) => {
+  return attrs.map(attr => attribute(attr)).join(' ');
 };
 
-const shades = function () {
+const createTag = function (tag, attributes, content) {
+  return openingTag(tag, attributes) + content + closingTag(tag);
+};
+
+const shade = (color) => {
+  const attrs = [
+    { attr: 'class', value: 'shade' },
+    { attr: 'style', value: bg(color) }
+  ];
+  // return openingTag('div', attributes(attrs)) + closingTag('div');
+  return createTag('div', attributes(attrs), '');
+};
+
+const shadesBgs = () => {
   const rgb = [randomInt(MAX_RGB), randomInt(MAX_RGB), randomInt(MAX_RGB)];
+  let alpha = 0.15;
   const inc = 0.10;
   const fractionDigits = 2;
-  let alpha = 0.15;
-  let divs = '';
+  const bgColors = [];
+
   do {
-    divs = shade(rgb, alpha) + divs;
+    bgColors.unshift({ rgb, alpha });
     alpha += inc;
-    alpha = +alpha.toFixed(fractionDigits);
+    alpha = Number(alpha.toFixed(fractionDigits));
   } while (alpha < MAX_ALPHA);
-  return divs;
+  return bgColors;
 };
+
+const shades = () => shadesBgs().map(bg => shade(bg)).join('');
+
+const link = () => '<link rel="stylesheet" href="styles.css"/>';
 
 const createPage = function () {
   const headContent = createTag('title', '', 'Color Shades') + link();
   const head = createTag('head', '', headContent);
-  const bodyContent = createTag('div', 'container', shades());
+  const containerAttrs = attributes([{
+    'attr': 'class',
+    'value': 'container'
+  }]);
+  const bodyContent = createTag('div', containerAttrs, shades());
   const body = createTag('body', '', bodyContent);
 
   return createTag('html', '', head + body);
